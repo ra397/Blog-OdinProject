8cafdbec256dc1b640b553e8c5126a9241536957
Rabi Alaya <rabi-alaya@uiowa.edu>
2024-10-14 15:19:54 -0500

    created a view that shows a single article, created member method show in the articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
     # fetch all articles
     @articles = Article.all
   end
+
+  def show
+    article_id = params[:id]
+    @article = Article.find(article_id)
+  end
 end
